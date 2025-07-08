de580e824b6998d3f6400e531b6ccdf21bd1a186
JamesGit994 <jamesjamneck@gmail.com>
2025-07-08 10:24:12 +0200

    Added sys module and error handle

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys
 from stats import get_book_text
 from stats import count_char
 from stats import dict_sorting
 
 def main():
-    path = "books/frankenstein.txt"
+    try:
+        if sys.argv[1]:
+            path = sys.argv[1]
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book_in_string = get_book_text(path)
     list_string = book_in_string.split()
     word_count = len(list_string)
@@ -12,7 +18,7 @@ def main():
     sorted_list = dict_sorting(new_dict)
     
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f'Found {word_count} total words')
     print("--------- Character Count -------")
